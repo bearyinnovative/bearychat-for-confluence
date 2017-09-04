21e24a098c3324531f2cca4333a7b0b29b69dc83
shonenada <shonenada@gmail.com>
2017-09-04 18:49:55 +0800

    chore(webhook): retab source files

--- src/main/java/com/bearychat/components/ConfigurationManager.java
+++ src/main/java/com/bearychat/components/ConfigurationManager.java
@@ -6,43 +6,50 @@ import com.atlassian.confluence.setup.bandana.ConfluenceBandanaContext;
 import com.bearychat.components.ConfigurationOption;
 
 public class ConfigurationManager {
-   private static final ConfluenceBandanaContext GLOBAL_CONTEXT = ConfluenceBandanaContext.GLOBAL_CONTEXT;
-   private BandanaManager                        bandanaManager;
+    private static final ConfluenceBandanaContext GLOBAL_CONTEXT = ConfluenceBandanaContext.GLOBAL_CONTEXT;
+    private BandanaManager                        bandanaManager;
 
-   public ConfigurationManager(BandanaManager bandanaManager) {
-      this.bandanaManager = bandanaManager;
-   }
+    public ConfigurationManager(BandanaManager bandanaManager) {
+        this.bandanaManager = bandanaManager;
+    }
 
-   public String getWebhookUrl() {
-      return getGlobalValue(ConfigurationOption.WEBHOOK_URL);
-   }
+    public String getWebhookUrl() {
+        return getGlobalValue(ConfigurationOption.WEBHOOK_URL);
+    }
 
-   public void setWebhookUrl(String webhookUrl) {
-      setGlobalValue(ConfigurationOption.WEBHOOK_URL, webhookUrl);
-   }
+    public void setWebhookUrl(String webhookUrl) {
+        setGlobalValue(ConfigurationOption.WEBHOOK_URL, webhookUrl);
+    }
 
-   private String getGlobalValue(ConfigurationOption option) {
-      return getBandanaValue(GLOBAL_CONTEXT, option);
-   }
+    private String getGlobalValue(ConfigurationOption option) {
+        return getBandanaValue(GLOBAL_CONTEXT, option);
+    }
 
-   private void setGlobalValue(ConfigurationOption option, String webhookUrl) {
-      bandanaManager.setValue(GLOBAL_CONTEXT, option.getBandanaKey(), webhookUrl);
-   }
+    private void setGlobalValue(ConfigurationOption option, String webhookUrl) {
+        bandanaManager.setValue(GLOBAL_CONTEXT, option.getBandanaKey(), webhookUrl);
+    }
 
-   public void setSpaceChannels(String spaceKey, String channels) {
-      bandanaManager.setValue(new ConfluenceBandanaContext(spaceKey), ConfigurationOption.CHANNELS.getBandanaKey(), channels);
-   }
+    public void setSpaceChannels(String spaceKey, String channels) {
+        bandanaManager.setValue(new ConfluenceBandanaContext(spaceKey), ConfigurationOption.CHANNELS.getBandanaKey(), channels);
+    }
 
-   public String getSpaceChannels(String spaceKey) {
-      return getBandanaValue(new ConfluenceBandanaContext(spaceKey), ConfigurationOption.CHANNELS);
-   }
+    public String getSpaceChannels(String spaceKey) {
+        return getBandanaValue(new ConfluenceBandanaContext(spaceKey), ConfigurationOption.CHANNELS);
+    }
 
-   private String getBandanaValue(BandanaContext bandanaContext, ConfigurationOption configurationOption) {
-      Object fromBandana = bandanaManager.getValue(bandanaContext, configurationOption.getBandanaKey());
-      if (fromBandana == null) {
-         return "";
-      }
-      return fromBandana.toString();
-   }
+    public void setSpaceWebhookUrl(String spaceKey, String webhookUrl) {
+        bandanaManager.setValue(new ConfluenceBandanaContext(spaceKey), ConfigurationOption.WEBHOOK_URL.getBandanaKey(), webhookUrl);
+    }
 
+    public String getSpaceWebhookUrl(String spaceKey) {
+        return getBandanaValue(new ConfluenceBandanaContext(spaceKey), ConfigurationOption.WEBHOOK_URL);
+    }
+
+    private String getBandanaValue(BandanaContext bandanaContext, ConfigurationOption configurationOption) {
+        Object fromBandana = bandanaManager.getValue(bandanaContext, configurationOption.getBandanaKey());
+        if (fromBandana == null) {
+            return "";
+        }
+        return fromBandana.toString();
+    }
 }
--- src/main/java/com/bearychat/components/ConfigurationOption.java
+++ src/main/java/com/bearychat/components/ConfigurationOption.java
@@ -1,15 +1,15 @@
 package com.bearychat.components;
 
 public enum ConfigurationOption {
-   WEBHOOK_URL("webhook.url"), CHANNELS("channels");
+    WEBHOOK_URL("webhook.url"), CHANNELS("channels");
 
-   private String suffix;
+    private String suffix;
 
-   private ConfigurationOption(String suffix) {
-      this.suffix = suffix;
-   }
+    private ConfigurationOption(String suffix) {
+        this.suffix = suffix;
+    }
 
-   public String getBandanaKey() {
-      return "bearychat." + suffix;
-   }
+    public String getBandanaKey() {
+        return "bearychat." + suffix;
+    }
 }
